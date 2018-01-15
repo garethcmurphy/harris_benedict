82ce39248a58a1940a07ced2ec905b0883d50580
garethcmurphy <garethcmurphy@gmail.com>
2018-01-15 21:00:59 +0100

    first commit

--- BaseMetabolicRate.py
+++ BaseMetabolicRate.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
-"""Foobar.py: Description of what foobar does."""
+"""BasalMetabolicRate.py: Calculates BMR."""
 
-__author__ = "Barack Obama"
+__author__ = "Gareth Murphy"
 
 
 class BaseMetabolicRate:
     weight = 85
-    height = 72
+    height = 172
     age = 37
+    totalheat = 1745
+    leanbodyweight = 0.79 * weight
 
     def bmr(self):
         bmr = self.weight * 10 + self.height * 6.25 - self.age * 5 + 5
         return bmr
+
+    def katchmacardle(self):
+        return 370 + 21.6* self.leanbodyweight
 
     def show(self):
         print(self.bmr())
@@ -21,3 +26,4 @@ class BaseMetabolicRate:
 if __name__ == '__main__':
     bmr = BaseMetabolicRate()
     bmr.show()
+    print (bmr.katchmacardle())
